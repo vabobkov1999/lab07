749b6c4acdec1f41f7f3f09fec6a98499b01aaaf
RomanVas30 <romanvas3008@gmail.com>
2018-05-27 22:59:31 +0000

    added documentation

---
+++ search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['print',['print',['../print_8hpp.html#ae12fd32fb711d3e888d4cd8a65974c65',1,'print(const std::string &amp;text, std::ostream &amp;out=std::cout):&#160;print.hpp'],['../print_8hpp.html#a88a4a55e7c5f7356f8a1fdcfbfbd1ae9',1,'print(const std::string &amp;text, std::ofstream &amp;out):&#160;print.hpp']]],
+  ['print_2ehpp',['print.hpp',['../print_8hpp.html',1,'']]]
+];
